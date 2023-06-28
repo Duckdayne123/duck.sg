e703120f48c4924641bde93e74f8667fb2922855
Duckdayne123 <115419886+Duckdayne123@users.noreply.github.com>
2023-06-28 12:08:05 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,45 +4,30 @@ const USER = {
       {
             name: "MB BANK",
             logo: "./images/mbbank.png",
-            number: "11112222456789",
-            owner: "NGUYEN HONG ANH DUC"
-        },
-              {
-            name: "BIDV",
-            logo: "./images/bidv.png",
-            number: "62010001446581",
-            owner: "NGUYEN HONG ANH DUC",
-            
-        },
-       
-      
-      {
-            name: "VIETCOMBANK",
-            logo: "./images/vcb.jpg",
-            number: "1032286524",
-            owner: "NGUYEN HONG ANH DUC"
+            number: "0339428367",
+            owner: "HOANG ANH DUC"
         },
       
       
       {
             name: "TP BANK",
             logo: "./images/tpbank.jpg",
-            number: "00000166098",
-            owner: "NGUYEN HONG ANH DUC"
+            number: "anhduc07",
+            owner: "HOANG ANH DUC"
         },
       
         {
             name: "MOMO",
             logo: "./images/momo.jpeg",
-            number: "0935723340",
-            owner: "NGUYỄN HỒNG ANH ĐỨC",
+            number: "0339428367",
+            owner: "HOÀNG ANH ĐỨC",
             fastTransfer: true
         },
                {
             name: "ZALO PAY",
             logo: "./images/zalopay.png",
-            number: "0935723340",
-            owner: "NGUYỄN HỒNG ANH ĐỨC",
+            number: "0339428367",
+            owner: "HOÀNG ANH ĐỨC",
             
         },
        
